bcfec37df47b855798e73b76a3ec8578eb4c9da4
tnt <45692264+tuna2134@users.noreply.github.com>
2022-02-18 21:52:21 +0900

    Add: typing

--- google_custom_search/search.py
+++ google_custom_search/search.py
@@ -2,6 +2,7 @@ import requests
 import aiohttp
 import json
 from .object import result
+from typing import Optional
 
 url = "https://www.googleapis.com/customsearch/v1"
 
@@ -15,37 +16,29 @@ class TokenError(Exception):
     pass
 
 class custom_search(object): 
-    def __init__(self, apikey=None, engine_id=None, image=False):
-        if apikey is None:
-            raise TokenError("apikey is invaid")
-        if engine_id is None:
-            raise Engine_IdError("engine_id is invaid")
-        else: 
-            self.token = apikey
-            self.engine_id = engine_id
-            self.image = image
+    def __init__(self,
+                 apikey: str,
+                 engine_id: str,
+                 image: Optional[bool]=False):
+        self.token = apikey
+        self.engine_id = engine_id
+        self.image = image
 
-    def search(self, keyword=None):
-        if keyword is None:
-            raise KeywordError("keyword is None")
-        else:
-            params={
-                "key": self.token,
-                "cx": self.engine_id,
-                "q": keyword
-            }
-            res = requests.get(url,params=params)
-            return result(res.json())
+    def search(self, keyword:str) -> result:
+        params={
+            "key": self.token,
+            "cx": self.engine_id,
+            "q": keyword
+        }
+        res = requests.get(url,params=params)
+        return result(res.json())
       
-    async def search_async(self, keyword=None):
-        if keyword is None:
-            raise KeywordError("keyword is None")
-        else:
-            params={
-                "key": self.token,
-                "cx": self.engine_id,
-                "q": keyword
-            }
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params) as res:
-                    return result(await res.json())
+    async def search_async(self, keyword:str) -> result:
+        params={
+            "key": self.token,
+            "cx": self.engine_id,
+            "q": keyword
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params) as res:
+                return result(await res.json())
