27737fbac01e6df7ab851083219fe3be1a89eb83
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-02-28 21:42:10 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../google_custom_search'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
