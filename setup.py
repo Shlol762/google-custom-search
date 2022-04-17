e116ea96034d47108f46d046bfa0fecb34ac9341
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-17 21:22:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ extras_require = {
 
 setuptools.setup(
     name="google-custom-search",
-    version="2.0.0",
+    version="2.0.1",
     author="DMS",
     author_email="masato190411@gmail.com",
     description="This is for google custom search api.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tuna2134/google-custom-search",
-    install_requires=_requires_from_file('rqs.txt'),
+    install_requires=_requires_from_file('requirements.txt'),
     extras_require=extras_require,
     packages=setuptools.find_packages(),
     classifiers=[
