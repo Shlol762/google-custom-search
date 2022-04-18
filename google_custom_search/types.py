7360d74eb441851be91d6c2dbaf484286eef06d4
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-18 16:35:44 +0900

    Update types.py

--- google_custom_search/types.py
+++ google_custom_search/types.py
@@ -4,34 +4,35 @@ import json
 
 class Item:
     """This is return a item."""
-    def __init__(self, data, **kwargs):
+    def __init__(self, data: dict):
         self.data: dict = data
             
     @property
     def kind(self) -> str:
+        "This is the kind of the item."
         return self.data["kind"]
     
     @property
     def title(self) -> str:
-        """This is the title of the item."""
+        "This is the title of the item."
         return self.data["title"]
     
     @property
     def url(self) -> str:
-        """This is the url of the item."""
+        "This is the url of the item."
         return self.data["link"]
     
     @property
     def display_url(self) -> str:
-        """This is the display url of the item."""
+        "This is the display url of the item."
         return self.data["displayLink"]
     
     @property
     def html_title(self) -> str:
-        """This is the html title of the item."""
+        "This is the html title of the item."
         return self.data["htmlTitle"]
     
     @property
     def snippet(self) -> str:
-        """This is the snippet of the item."""
+        "This is the snippet of the item."
         return self.data["snippet"]
