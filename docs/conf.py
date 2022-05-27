00190b33f0b7c43af6b94161fdeef32735115980
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-27 17:00:07 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, dms'
 author = 'dms'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.4'
+release = '2.0.6'
 
 
 # -- General configuration ---------------------------------------------------
