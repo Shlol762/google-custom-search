27027c6f39cb392d0fc4f2f0aafecb3f9d8911e4
mc_fdc <Masato.04.11.2007@gmail.com>
2022-10-15 12:36:27 +0900

    Update __init__.py

--- google_custom_search/__init__.py
+++ google_custom_search/__init__.py
@@ -1,6 +1,7 @@
-__version__ = "2.0.9"
-# __all__ = ("AsyncError", "ApiNotEnabled", "custom_search", "Item")
-
 from .search import *
 from .types import *
 from .errors import *
+
+
+__version__ = "2.0.10a1"
+__author__ = "mc-fdc-dev"
